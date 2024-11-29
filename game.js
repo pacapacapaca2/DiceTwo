a684f297003448735e1150b0d7b97f966822a0cd
pacapacapaca2 <163531602+pacapacapaca2@users.noreply.github.com>
2024-11-29 10:48:20 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+let playerPosition = 0;
+
+document.getElementById('roll-dice').addEventListener('click', () => {
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    playerPosition += diceRoll;
+
+    document.getElementById('position').innerText = playerPosition;
+
+    alert(`You rolled: ${diceRoll}. New position: ${playerPosition}`);
+});
