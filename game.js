cc493e66baaac6e1ed534b8c8f96799715455ba5
StanB <stanb791@gmail.com>
2024-12-01 12:59:20 +0300

    nocomm

--- game.js
+++
@@ -1,8 +0,0 @@
-let playerPosition = 0;
-
-document.getElementById('roll-dice').addEventListener('click', () => {
-    const diceRoll = Math.floor(Math.random() * 6) + 1;
-    playerPosition += diceRoll;
-
-    document.getElementById('position').innerText = playerPosition;
-});
