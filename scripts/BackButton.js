e65356c76134256e67b64d15d285a389688c1bc7
pacapacapaca2 <163531602+pacapacapaca2@users.noreply.github.com>
2024-12-01 10:17:40 +0300

    Delete scripts directory

--- scripts/BackButton.js
+++
@@ -1,6 +0,0 @@
-const tg = window.Telegram.WebApp;
-        tg.BackButton.show();
-        tg.BackButton.onClick(() => {
-            class="_link_lyone_12" href="/DiceTwo/"; 
-        });
-        tg.expand();
